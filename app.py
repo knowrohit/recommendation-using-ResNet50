123a94ae1a61dc71a72f719886832df728221477
Rohit Tiwari <48828304+knowrohit@users.noreply.github.com>
2022-06-27 21:57:52 +0530

    need to change predict features

---
+++ app.py
@@ -0,0 +1,46 @@
+# we call the get_recommender function from bodega_cnn file and store the 
+#recommendations in a variable called res. The loaded imagee uois passed
+#trhough request.args.get() and parameters are figures.
+# finding ways to return images as result and how to input the reference image on 
+#on terminal 
+# we need a html index to submit image as a form 
+import numpy as np
+from flask import Flask, request, jsonify, render_template, url_for
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+
+
+@app.route('/')
+def home():
+    #return 'Hello World'
+    return render_template('index.html')
+    #return render_template('index.html')
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    print(prediction[0])
+
+    #output = round(prediction[0], 2)
+    return render_template('index.html', prediction_text="Bodega_Recommendation engine {}".format(prediction[0]))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
